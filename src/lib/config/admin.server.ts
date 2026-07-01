96d536a84740f8a613bfef25e181aa027be1e646
Jesse Pomeroy <thinkingofview@gmail.com>
2026-07-01 16:58:19 -0400

    Use shared admin auth validator (#36)

--- src/lib/config/admin.server.ts
+++ src/lib/config/admin.server.ts
@@ -1,8 +1,7 @@
-import { type AdminServerConfig, cookiesFromRequest } from "@jessepomeroy/admin/server";
-import { getToken } from "@mmailaender/convex-better-auth-svelte/sveltekit";
+import type { AdminServerConfig } from "@jessepomeroy/admin/server";
 import { env as privateEnv } from "$env/dynamic/private";
 import { env as publicEnv } from "$env/dynamic/public";
-import { requireAuth } from "$lib/server/adminAuth";
+import { adminAuth } from "$lib/server/adminAuth";
 import { getGalleryWorkerUrl } from "$lib/server/galleryWorkerUrl";
 import { adminConfig } from "./admin";
 
@@ -12,11 +11,6 @@ export const adminServerConfig: AdminServerConfig = {
 	resendApiKey: privateEnv.RESEND_API_KEY ?? "",
 	galleryWorkerUrl: getGalleryWorkerUrl(),
 	galleryAdminSecret: privateEnv.GALLERY_ADMIN_SECRET ?? "",
-	verifyAdmin: async (request) => {
-		await requireAuth(cookiesFromRequest(request));
-		return true;
-	},
-	getConvexToken: async (request) => {
-		return getToken(cookiesFromRequest(request)) ?? null;
-	},
+	verifyAdmin: adminAuth.verifyRequest,
+	getConvexToken: adminAuth.getTokenFromRequest,
 };
