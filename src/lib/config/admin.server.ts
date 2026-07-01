e4e0230af007a5bef37542f0974c9a78112d5087
jessepomeroy <thinkingofview@gmail.com>
2026-07-01 12:24:26 -0400

    Adopt admin host cookie helper

--- src/lib/config/admin.server.ts
+++ src/lib/config/admin.server.ts
@@ -1,26 +1,10 @@
-import type { AdminServerConfig } from "@jessepomeroy/admin";
+import { type AdminServerConfig, cookiesFromRequest } from "@jessepomeroy/admin";
 import { getToken } from "@mmailaender/convex-better-auth-svelte/sveltekit";
-import type { Cookies } from "@sveltejs/kit";
 import { env as privateEnv } from "$env/dynamic/private";
 import { env as publicEnv } from "$env/dynamic/public";
 import { requireAuth } from "$lib/server/adminAuth";
 import { getGalleryWorkerUrl } from "$lib/server/galleryWorkerUrl";
 import { adminConfig } from "./admin";
-
-function cookiesFromRequest(request: Request): Cookies {
-	const cookieHeader = request.headers.get("cookie") ?? "";
-	const cookies = Object.fromEntries(
-		cookieHeader
-			.split(";")
-			.map((cookie) => cookie.trim())
-			.filter(Boolean)
-			.map((cookie) => {
-				const [name, ...rest] = cookie.split("=");
-				return [name, rest.join("=")];
-			}),
-	);
-	return { get: (name: string) => cookies[name] } as Cookies;
-}
 
 export const adminServerConfig: AdminServerConfig = {
 	...adminConfig,
