6b58fedc3f02ac4323e618cec31fc0d26de15d2c
Jesse Pomeroy <thinkingofview@gmail.com>
2026-07-16 11:11:42 -0400

    feat(editor): adopt portfolio gallery workspace (#105)

---
+++ src/routes/admin/editor/portfolio/[galleryId]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { PortfolioGalleryPage } from "@jessepomeroy/admin";
+import type { PageData } from "./$types";
+
+let { data }: { data: PageData } = $props();
+</script>
+
+<PortfolioGalleryPage galleryId={data.galleryId} />
