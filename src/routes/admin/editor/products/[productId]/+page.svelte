65f92eab236527671dfc0198cf39ffae4193dfea
Jesse Pomeroy <thinkingofview@gmail.com>
2026-07-21 12:11:43 -0400

    fix(editor): expose product routes (#133)

---
+++ src/routes/admin/editor/products/[productId]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { ProductPage } from "@jessepomeroy/admin";
+import type { PageData } from "./$types";
+
+let { data }: { data: PageData } = $props();
+</script>
+
+<ProductPage productId={data.productId} />
